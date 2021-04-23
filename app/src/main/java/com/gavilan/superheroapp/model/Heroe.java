c6608ff2b3d931fb1709c956ee4c4448673abd47
Jotape <j.p.daniel.gavilan@gmail.com>
2021-04-22 20:08:29 -0400

    V 0.1 SuperHeroes

---
+++ app/src/main/java/com/gavilan/superheroapp/model/Heroe.java
@@ -0,0 +1,58 @@
+package com.gavilan.superheroapp.model;
+
+public class Heroe {
+    /*
+        - id
+        - Nombre
+        - Poder
+        - Mundo
+        - Peliculas en la participo **
+        - Edad
+
+     */
+    private int idHeroe;
+    private String nombreHeroe;
+    private String superPoder;
+    private String mundo;
+    private int edad;
+
+    public int getIdHeroe() {
+        return idHeroe;
+    }
+
+    public void setIdHeroe(int idHeroe) {
+        this.idHeroe = idHeroe;
+    }
+
+    public String getNombreHeroe() {
+        return nombreHeroe;
+    }
+
+    public void setNombreHeroe(String nombreHeroe) {
+        this.nombreHeroe = nombreHeroe;
+    }
+
+    public String getSuperPoder() {
+        return superPoder;
+    }
+
+    public void setSuperPoder(String superPoder) {
+        this.superPoder = superPoder;
+    }
+
+    public String getMundo() {
+        return mundo;
+    }
+
+    public void setMundo(String mundo) {
+        this.mundo = mundo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
---
+++ app/src/main/java/com/gavilan/superheroapp/model/HeroeAdapter.java
@@ -0,0 +1,45 @@
+package com.gavilan.superheroapp.model;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.gavilan.superheroapp.R;
+
+import java.util.ArrayList;
+
+public class HeroeAdapter extends RecyclerView.Adapter<HeroeAdapter.ViewHolderDatos>  {
+
+    ArrayList<Heroe> ListaHeroes; // lista que queremos cargar
+    public HeroeAdapter(ArrayList<Heroe> heroes){
+        ListaHeroes =  heroes;
+    }
+
+    @NonNull
+    @Override
+    public HeroeAdapter.ViewHolderDatos onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_heroe,null,false);
+
+        return new ViewHolderDatos(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HeroeAdapter.ViewHolderDatos holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return ListaHeroes.size();
+    }
+
+    public class ViewHolderDatos extends RecyclerView.ViewHolder {
+        public ViewHolderDatos(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
